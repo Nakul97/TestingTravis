434f70a68d9853f2948405d9a81b7c830e6e8dad
Nakul Sakhuja <nakul.sakhuja@zomato.com>
2019-03-14 15:37:55 +0530

    new file

---
+++ TestingTravis/testClass.swift
@@ -0,0 +1,17 @@
+//
+//  testClass.swift
+//  TestingTravis
+//
+//  Created by Zomato on 14/03/19.
+//  Copyright © 2019 tester. All rights reserved.
+//
+
+import Foundation
+
+class testClass: NSObject {
+    
+    override init() {
+        
+    }
+    
+}
