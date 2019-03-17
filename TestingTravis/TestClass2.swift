7ca9f25deb75f5cbfac5fdece755e1b84114f26c
Nakul Sakhuja <nakul.sakhuja@zomato.com>
2019-03-17 11:10:44 +0530

    testing danger

---
+++ TestingTravis/TestClass2.swift
@@ -0,0 +1,16 @@
+//
+//  TestClass2.swift
+//  TestingTravis
+//
+//  Created by Zomato on 17/03/19.
+//  Copyright © 2019 tester. All rights reserved.
+//
+
+import Foundation
+
+class TestClass2{
+    
+    init(){
+        let unusedVar2 = 0
+    }
+}
